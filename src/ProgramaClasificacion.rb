0444ab9324d0c0998e6572f0e5d10e1d0ac3bf93
Isaac Aiman <alu0100841565@ull.edu.es>
2017-05-07 09:27:40 +0100

    Fixed ProgramaClasificacion.rb

--- src/ProgramaClasificacion.rb
+++ src/ProgramaClasificacion.rb
@@ -9,56 +9,73 @@ ARGV.each do|filename|
 end
 
 # Abrimos todos los ficheros de aprendizaje
+$files = []
+(1..CATEGORIES).each do |i|
+    $files << File.read("./../generatedFiles/aprendizaje" + i.to_s + ".txt").split(/\n/)
+end
 
 # Abrimos el fichero vocabulario.txt
 $vocabulario = File.read("./../generatedFiles/vocabulario.txt").split(/\n/)
 
+# Calculamos el número de palabras del vocabulario.
+$num_palabras_vocabulario = $vocabulario[0].match(/palabras: (.*)/)[1]
 
-probabilidades = []
-
-# Pasa un corpus a hash
-def probabilidad_palabra(num_fichero, palabra, num_palabras_vocabulario, num_palabras_corpus)
-
-  $files[num_fichero].each do |line|
-    key = line.match(/Palabra: (.*) Frec/)[1]
-    value = line.match(/LogProb: (.*)/)[1]
-
-    if (key == palabra)
-      return value.to_f
+# Array con todas las tablas hash (una tabla hash por fichero de aprendizaje)
+$tablas_hash = []
+# Pasa todos los ficheros de aprendizaje a una tabla hash
+def to_hash()
+  (1..CATEGORIES).each do |i|
+    file =  File.read("./../generatedFiles/aprendizaje" + i.to_s + ".txt").split(/\n/)
+    hash = {}
+    file.each do |line|
+      if ((line =~ /Palabra/) != nil)
+        key = line.match(/Palabra: (.*) Frec/)[1]
+        value = line.match(/LogProb: (.*)/)[1]
+      end
+      hash[key] = value
     end
+    $tablas_hash << hash
   end
+end
 
-  return Math::log(( 1.0 / ( num_palabras_corpus.to_f + num_palabras_vocabulario.to_f + 1).to_f), 2)
+# Calcula la probabilidad de una palabra usando un fichero de aprendizaje determinado.
+def probabilidad_palabra(num_fichero, palabra, num_palabras_corpus)
+
+  if ($tablas_hash[num_fichero][palabra] != nil)
+    return $tablas_hash[num_fichero][palabra].to_f
+  end
+  return Math::log(( 1.0 / ( num_palabras_corpus.to_f + $num_palabras_vocabulario.to_f + 1).to_f), 2)
 
 end
 
 # Devuelve la categoria a la que pertenece
 def calcular_probabilidad(line, num_fichero)
 
-  num_palabras_vocabulario = $vocabulario[0].match(/palabras: (.*)/)[1]
-  $files[num_fichero].delete_at(0)
-  num_palabras_corpus = $files[num_fichero][0].match(/Número de palabras del corpus: (.*)/)[1]
-  $files[num_fichero].delete_at(0)
+  num_palabras_corpus = $files[num_fichero][1].match(/Número de palabras del corpus: (.*)/)[1]
 
-  probabilidad = 0
+  probabilidad = 0.0
   line.split(/[^[[:word:]]]/).reject(&:empty?).each do |word|
-    probabilidad += probabilidad_palabra(num_fichero, word, num_palabras_vocabulario, num_palabras_corpus)
+    probabilidad += probabilidad_palabra(num_fichero, word, num_palabras_corpus)
   end
 
   return probabilidad
 end
 
 
+to_hash()
+
 $corpus.each do |line|
-  $files = []
-  (1..CATEGORIES).each do |i|
-      $files << File.read("./../generatedFiles/aprendizaje" + i.to_s + ".txt").split(/\n/)
-  end
+  mayor = calcular_probabilidad(line, 0).to_f
+  pos = 0
+  (1..CATEGORIES - 1).each do |num_fichero|
+    prob = calcular_probabilidad(line, num_fichero).to_f
 
-  mayor = []
-  (0..CATEGORIES - 1).each do |num_fichero|
-    mayor << calcular_probabilidad(line, num_fichero).to_s
-  end
+    if (prob > mayor)
+      mayor = prob
+      pos = num_fichero
+    end
 
-  puts line + " " + mayor.each_with_index.max[1].to_s
+  end
+  puts line + " " + (pos + 1).to_s
 end
+
